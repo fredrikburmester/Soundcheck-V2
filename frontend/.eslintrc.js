6fd4417073144b75ce6f85d43a6bbfbb8384cc4c
Fredrik Burmester <fredrik.burmester@gmail.com>
2022-04-06 23:36:14 +0200

    change to warnings

--- frontend/.eslintrc.js
+++ frontend/.eslintrc.js
@@ -7,5 +7,7 @@ module.exports = {
         // override/add rules settings here, such as:
         // 'vue/no-unused-vars': 'error'
         'vue/require-default-prop': 'off',
+        'no-unused-vars': [1],
+        'vue/no-unused-components': [1],
     },
 }
