b70644dbb9cc95d7f3ab0eeaf7a9bcac7cb0d951
Fredrik Burmester <fredrik.burmester@gmail.com>
2022-04-08 12:41:51 +0200

    feat: pwa

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -6,11 +6,13 @@ import vueJsx from '@vitejs/plugin-vue-jsx'
 
 import eslintPlugin from 'vite-plugin-eslint'
 
+import { VitePWA } from 'vite-plugin-pwa'
+
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => {
     const env = loadEnv(mode, process.cwd())
     return {
-        plugins: [vue(), vueJsx(), eslintPlugin()],
+        plugins: [vue(), vueJsx(), eslintPlugin(), VitePWA({})],
         resolve: {
             alias: {
                 '@': fileURLToPath(new URL('./src', import.meta.url)),
