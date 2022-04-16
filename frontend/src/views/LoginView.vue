d65a7407d957fca26d8b72df9078deaee8b574fa
Fredrik Burmester <fredrik.burmester@gmail.com>
2022-04-16 17:43:05 +0200

    fix: update info on login screen to be more informative

--- frontend/src/views/LoginView.vue
+++ frontend/src/views/LoginView.vue
@@ -1,39 +1,37 @@
 <template>
-	<div class="hero min-h-screen min-h-9/10">
-		<div class="hero-content flex-col lg:flex-row-reverse w-full">
-			<div class="text-center lg:text-left lg:ml-8">
-				<h1 class="text-5xl font-bold">Soundcheck</h1>
-				<p class="py-6">Welcome to Soundcheck V2! The best music quiz there is!</p>
-			</div>
-			<div
-				class="card flex-shrink-0 w-full max-w-sm shadow-2xl bg-base-300"
-			>
-				<div class="card-body">
-					<div class="form-control">
-						<button
-							class="btn btn-success text-base-100"
-							@click="login"
-						>
-							Login with spotify
-						</button>
-					</div>
-				</div>
-			</div>
-		</div>
-	</div>
+    <div class="hero min-h-screen min-h-8/10">
+        <div class="hero-content flex-col lg:flex-row-reverse w-full">
+            <div class="text-center lg:text-left lg:ml-8">
+                <h1 class="text-5xl font-bold">Soundcheck</h1>
+                <p class="py-6 max-w-md text-center">
+                    <span class="text-primary">Soundcheck</span>
+                    is a game to play with your friends! Create a room, invite your friends, and guess each others favorite songs.
+                    <span class="font-bold"> Listen to music, chat and have fun together!</span> <br /><br />
+                    After the game you can create a mix-tape of all your favorite songs, perfect for a party!
+                </p>
+            </div>
+            <div class="card flex-shrink-0 w-full max-w-sm shadow-2xl bg-base-300">
+                <div class="card-body">
+                    <div class="form-control">
+                        <button class="btn btn-success text-base-100" @click="login">Login with spotify</button>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
 </template>
 
 <script>
 export default {
-	methods: {
-		login() {
-			this.$socket.client.emit('login-step-1')
-		},
-	},
-	sockets: {
-		login(url) {
-			window.location.href = url
-		},
-	},
+    methods: {
+        login() {
+            this.$socket.client.emit('login-step-1')
+        },
+    },
+    sockets: {
+        login(url) {
+            window.location.href = url
+        },
+    },
 }
 </script>
