98298a803713617eb63d6ae2d6e86f5a602b5cb6
Fredrik Burmester <fredrik.burmester@gmail.com>
2022-08-14 18:53:34 +0200

    margins

--- frontend/src/components/LoadingComponent.vue
+++ frontend/src/components/LoadingComponent.vue
@@ -1,5 +1,5 @@
 <template>
-    <div class="flex flex-col items-center place-content-center">
+    <div class="flex flex-col items-center place-content-center my-12">
         <div class="hollow-dots-spinner">
             <div class="dot"></div>
             <div class="dot"></div>
@@ -35,7 +35,7 @@ export default {
     width: 15px;
     height: 15px;
     margin: 0 calc(15px / 2);
-    border: calc(15px / 5) solid v-bind(color);
+    border: 4px solid v-bind(color);
     border-radius: 50%;
     float: left;
     transform: scale(0);
@@ -43,15 +43,15 @@ export default {
 }
 
 .hollow-dots-spinner .dot:nth-child(1) {
-    animation-delay: calc(300ms * 1);
+    animation-delay: calc(150ms * 1);
 }
 
 .hollow-dots-spinner .dot:nth-child(2) {
-    animation-delay: calc(300ms * 2);
+    animation-delay: calc(150ms * 2);
 }
 
 .hollow-dots-spinner .dot:nth-child(3) {
-    animation-delay: calc(300ms * 3);
+    animation-delay: calc(150ms * 3);
 }
 @keyframes hollow-dots-spinner-animation {
     50% {
