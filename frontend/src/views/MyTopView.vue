62c9463340248752962c13d309c605438174e642
Fredrik Burmester <fredrik.burmester@gmail.com>
2022-08-14 18:58:37 +0200

    updated func for new design and database

--- frontend/src/views/MyTopView.vue
+++ frontend/src/views/MyTopView.vue
@@ -2,12 +2,7 @@
     <div>
         <div :key="item_type" class="flex flex-col px-8 pb-24 w-screen items-center">
             <div class="pb-8 max-w-2xl w-full">
-                <h1 class="text-3xl text-start">Your top {{ item_type }}</h1>
-                <p v-if="item_type == 'tracks'" class="text-start">Check your top Spotify tracks the last month, half year and more than a year!</p>
-                <p v-if="item_type == 'artists'" class="text-start">
-                    Here you can see your favorite Spotify artists the last month, half year and more than a year.
-                </p>
-                <br />
+                <h1 class="text-3xl text-start font-bold">Your top {{ item_type }}</h1>
                 <p>
                     <i class="text-gray-400"> Make sure to change the time range! </i>
                 </p>
@@ -32,48 +27,41 @@
                         </div>
                     </div>
                 </div>
-
                 <div class="tabs tabs-boxed bg-zinc-900 shadow-lg mt-4">
                     <a class="tab bg-zinc-900 rounded-l-lg" @click="toggleActive($event, 'short_term')">Month</a>
                     <a class="tab tab-active bg-zinc-900" @click="toggleActive($event, 'medium_term')">Half Year</a>
                     <a class="tab bg-zinc-900 rounded-r-lg" @click="toggleActive($event, 'long_term')">Over a year</a>
                 </div>
             </div>
-            <div v-if="!loading && items[item_type][time_range].length > 0" class="flex flex-col place-items-center w-full max-w-2xl">
+            <div v-if="!loading" class="flex flex-col place-items-center w-full max-w-2xl list">
                 <div v-if="item_type == 'tracks'">
                     <TransitionGroup name="list" tag="div">
                         <SongCard
-                            v-for="(item, index) in items[item_type][time_range]"
-                            :key="item"
-                            :img="item.album.images[0].url"
-                            :artist="item.artists[0].name"
-                            :index="index"
-                            :title="item.name"
+                            v-for="item in filteredItems(items)"
+                            :key="item.uuid"
+                            :song="item"
+                            :unique="isUnique(item.data.popularity)"
+                            :oldest="isOldest(item.dateAdded)"
                             class="mb-4"
                         ></SongCard>
                     </TransitionGroup>
                 </div>
-                <div v-if="item_type == 'artists'">
-                    <TransitionGroup name="list" tag="div">
-                        <SongCard
-                            v-for="(item, index) in items[item_type][time_range]"
-                            :key="item.id"
-                            :title="item.name"
-                            :index="index"
-                            :img="item.images[0].url"
-                            class="mb-4"
-                        ></SongCard>
-                    </TransitionGroup>
+                <div v-else>
+                    <ArtistCard
+                        v-for="item in filteredItems(items)"
+                        :key="item"
+                        :unique="isUnique(item.data.popularity)"
+                        :oldest="isOldest(item.dateAdded)"
+                        :artist="item"
+                        class="mb-4"
+                    ></ArtistCard>
                 </div>
-            </div>
-            <div v-if="!loading && items[item_type][time_range].length == 0" class="flex flex-col p-8">
-                <p>
-                    You have no {{ item_type }} for this period.
-                    <br />
-                    Try another period at the bottom.
-                </p>
             </div>
             <LoadingComponent v-if="loading" />
+            <button v-if="!loadingMore && !loading" class="btn btn-sm btn-success" @click="loadMore">Load more</button>
+
+            <!-- daisy ui spinner -->
+            <LoadingComponent v-if="loadingMore" />
         </div>
     </div>
 </template>
@@ -82,49 +70,49 @@ import { useUserStore } from '@/stores/user'
 import { mapWritableState, mapActions } from 'pinia'
 import SongCard from '../components/SongCardComponent.vue'
 import LoadingComponent from '../components/LoadingComponent.vue'
+import ArtistCard from '../components/ArtistCardComponent.vue'
 
 export default {
-    components: { SongCard, LoadingComponent },
+    components: { SongCard, LoadingComponent, ArtistCard },
     data() {
         return {
             item_type: this.$route.params.id,
             time_range: 'medium_term',
-            items: {
-                tracks: {
-                    short_term: [],
-                    medium_term: [],
-                    long_term: [],
-                },
-                artists: {
-                    short_term: [],
-                    medium_term: [],
-                    long_term: [],
-                },
-            },
+            items: [],
             loading: true,
             playlistName: '',
+            loadingMore: false,
+            amount: 25,
+            lowestPopularity: 0,
+            lowestPopularityIndex: 0,
+            oldestDate: null,
         }
     },
     computed: {
         ...mapWritableState(useUserStore, ['refresh_token', 'token', 'top_items']),
     },
     watch: {
-        $route(oldVal, newVal) {
+        async $route(oldVal, newVal) {
             if (this.$route.name == 'MyTop') {
                 if (newVal.params.id == 'artists' || newVal.params.id == 'tracks') {
                     this.item_type = this.$route.params.id
                     this.time_range = 'medium_term'
-                    this.getItems()
-                    this.loading = false
+                    const topItems = await this.getTopSongs(this.time_range, 25, this.item_type)
+                    console.log('[switched route]', topItems)
+                    this.getTopItemsFromBackend(topItems, this.time_range, this.item_type)
                 }
             }
         },
     },
     async mounted() {
         this.loading = true
-        await this.getItems()
+        // await this.getItems()
+        const topItems = await this.getTopSongs(this.time_range, 25, this.item_type)
+        console.log('[mounted]', topItems)
+        this.getTopItemsFromBackend(topItems, this.time_range, this.item_type)
     },
     methods: {
+        ...mapActions(useUserStore, ['getTopSongs', 'createPlaylist']),
         getTimeRangePrettyString(time_range) {
             if (time_range == 'short_term') {
                 return 'Last month'
@@ -147,16 +135,25 @@ export default {
             const year = date.getFullYear().toString()
             return `${month}/${year.substring(2, 4)}`
         },
+        filteredItems(items) {
+            const type = this.item_type == 'tracks' ? 'track' : 'artist'
+            const filteredItems = items.filter((item) => item.timeRange == this.time_range && item.type == type)
+
+            // sort items based in index
+            const sortedItems = filteredItems.sort((a, b) => {
+                return a.index - b.index
+            })
+            console.log('[sorted items]', sortedItems)
+            return sortedItems
+        },
         setPlaylistName() {
             this.playlistName = `My Top ${this.getItemTypePrettyString(this.item_type)} - ${this.getTimeRangePrettyString(
                 this.time_range
             )} (${this.getPrettyDate()})`
         },
         compileAndCreatePlaylist() {
-            let tracks = this.items[this.item_type][this.time_range]
-            this.createPlaylist(tracks, this.playlistName)
+            this.createPlaylist(this.filteredItems(this.items), this.playlistName)
         },
-        ...mapActions(useUserStore, ['getTopSongs', 'createPlaylist']),
         async toggleActive(e, time_range) {
             this.loading = true
             Array.from(document.getElementsByClassName('tab')).forEach((element) => {
@@ -164,74 +161,92 @@ export default {
             })
             e.target.classList.toggle('tab-active')
 
-            // Get top songs from store
-            this.items[this.item_type][time_range] = await this.getTopSongs(time_range, 25, this.item_type)
-
             // Set time range
             this.time_range = time_range
+            this.amount = 25
 
-            // stop loading
-            this.loading = false
-
-            await this.sendTopSongIdsToBackend()
+            const topItems = await this.getTopSongs(this.time_range, this.amount, this.item_type)
+            this.getTopItemsFromBackend(topItems, this.time_range, this.item_type)
         },
-        async getItems() {
-            this.loading = true
-            this.items[this.item_type][this.time_range] = await this.getTopSongs(this.time_range, 25, this.item_type)
-            this.loading = false
-            await this.sendTopSongIdsToBackend()
+
+        getTopItemsFromBackend(topItems, timeRange, itemType) {
+            this.$socket.client.emit('getTopItemsFromBackend', {
+                topItems: topItems,
+                itemType: itemType,
+                timeRange: timeRange,
+            })
         },
-        async sendTopSongIdsToBackend() {
-            console.log('Sending top song ids to backend')
-            if (this.item_type == 'tracks') {
-                const tracks = this.items['tracks'][this.time_range]
-                console.log(tracks)
-
-                // Create object pair with track ids and index
-                const trackIdsWithIndex = tracks.map((track, index) => {
-                    return {
-                        id: track.id,
-                        index: index,
-                    }
-                })
-
-                const time_range = this.time_range
-
-                this.$socket.client.emit('topTracksForStoring', {
-                    trackIdsWithIndex: trackIdsWithIndex,
-                    time_range: time_range,
-                })
-            }
+        async loadMore() {
+            this.loadingMore = true
+            this.amount += 5
+            const topItems = await this.getTopSongs(this.time_range, this.amount, this.item_type)
+            this.getTopItemsFromBackend(topItems, this.time_range, this.item_type)
+        },
+        isUnique(popularity) {
+            return popularity == this.lowestPopularity || popularity <= 20
+        },
+        isOldest(date) {
+            return date == this.oldestDate
         },
     },
     sockets: {
-        refreshToken(data) {
-            this.token = data.token
+        sendTopItemsFromBackend(topItems) {
+            let lowestPopularity = 100
+            let oldestDate = new Date()
+            for (let i = 0; i < topItems.length; i++) {
+                const song = topItems[i]
+
+                if (song.timeRange != this.time_range) {
+                    continue
+                }
+
+                // Check for the oldest song
+                if (new Date(song.dateAdded) < oldestDate) {
+                    oldestDate = song.dateAdded
+                }
+
+                // Check the least popular song
+                if (song.data.popularity < lowestPopularity) {
+                    lowestPopularity = song.data.popularity
+                }
+            }
+
+            this.lowestPopularity = lowestPopularity
+            this.oldestDate = oldestDate
+
+            this.items = topItems
+
+            console.log('[from backend]', this.items)
+            console.log('[oldest date]', oldestDate)
+            console.log('[lowest popularity]', lowestPopularity)
+
+            this.loading = false
+            this.loadingMore = false
         },
     },
 }
 </script>
 <style scoped>
+.list {
+}
 .tabs {
     width: 278px;
     margin-left: -3px;
+}
+.list-leave-active {
+    opacity: 0;
+    position: absolute;
 }
 
 .list-move, /* apply transition to moving elements */
 .list-enter-active,
 .list-leave-active {
-    transition: all 0.5s ease;
+    transition: all 1s ease;
 }
 
 .list-enter-from,
 .list-leave-to {
-    opacity: 0;
+    opacity: 1;
     transform: translateX(30px);
-}
-
-/* ensure leaving items are taken out of layout flow so that moving
-   animations can be calculated correctly. */
-.list-leave-active {
-    position: absolute;
 }
 </style>
