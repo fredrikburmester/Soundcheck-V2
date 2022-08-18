5716095855ea48a3aa8dbf15569021d515a843f0
Fredrik Burmester <fredrik.burmester@gmail.com>
2022-08-18 15:18:51 +0200

    update home page design

--- frontend/src/views/HomeView.vue
+++ frontend/src/views/HomeView.vue
@@ -10,13 +10,13 @@
         </div>
         <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 auto-cols-max gap-4 md:flex-row flex-wrap">
             <FancyCardComponent link="/my-top/tracks">
-                <template #header>My top Spotify tracks</template>
-                <template #body> What do you listen to the most? </template>
+                <template #header>My Top Songs</template>
+                <template #body>What do you listen to the most?</template>
                 <template #action>Check it out</template>
             </FancyCardComponent>
             <FancyCardComponent link="/my-top/artists">
-                <template #header>My top Spotify artists</template>
-                <template #body> Are you a real fan? </template>
+                <template #header>My Top Artists</template>
+                <template #body>Are you a real fan?</template>
                 <template #action>Check it out</template>
             </FancyCardComponent>
             <hr class="md:hidden opacity-10 my-4" />
@@ -24,6 +24,12 @@
                 <template #header>Soundcheck!</template>
                 <template #body>Can you guess your friends favorite songs?</template>
                 <template #action>Play</template>
+            </FancyCardComponent>
+            <hr class="md:hidden opacity-10 my-4" />
+            <FancyCardComponent link="/my-top/artists">
+                <template #header>My Top Genres</template>
+                <template #body>Pop, rock, metal?</template>
+                <template #action>Check it out</template>
             </FancyCardComponent>
         </div>
         <hr class="my-8 opacity-10" />
