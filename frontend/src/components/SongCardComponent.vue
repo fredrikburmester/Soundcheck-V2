000a7d6092038c66d6f3606e1e27e1293894a3b9
Fredrik Burmester <fredrik.burmester@gmail.com>
2022-08-18 15:18:11 +0200

    design fixes

--- frontend/src/components/SongCardComponent.vue
+++ frontend/src/components/SongCardComponent.vue
@@ -2,24 +2,19 @@
     <div>
         <transition name="fade" mode="out-in">
             <div v-show="loaded" class="indicator">
-                <!-- <span v-if="newSong()" class="indicator-item indicator-top indicator-start badge badge-info mt-36">New</span> -->
-                <span v-if="unique && !(!newSong() && oldest)" class="indicator-item badge badge-error z-100">Rare</span>
-                <span v-if="!newSong() && oldest && !unique" class="indicator-item badge badge-secondary z-100">Old</span>
-                <span v-if="song.previousIndex != null && song.previousIndex > song.index" class="indicator-item indicator-start badge badge-success">↑</span>
-                <span v-if="song.previousIndex != null && song.previousIndex < song.index" class="indicator-item indicator-start badge badge-error">↓</span>
-
-                <template v-if="unique && !newSong() && oldest">
-                    <span v-if="unique" class="indicator-item indicator-end badge badge-error z-100">Rare</span>
-                    <span v-if="!newSong() && oldest" class="indicator-item badge badge-secondary z-100 -translate-x-8">Old</span>
-                </template>
+                <span v-if="previousIndex == null && newSong()" class="indicator-item indicator-top indicator-start badge badge-info mt-36">New</span>
+                <span v-if="previousIndex != null && previousIndex > index" class="indicator-item indicator-start badge badge-success">↑</span>
+                <span v-if="previousIndex != null && previousIndex < index" class="indicator-item indicator-start badge badge-error">↓</span>
+                <span v-if="unique" class="indicator-item badge badge-error z-100">Rare</span>
+                <span v-if="oldest == song.uuid" class="indicator-item badge badge-warning z-100">Old</span>
 
                 <div class="card max-w-2xl bg-base-100 shadow-xl image-full mb-4" @click="expand">
                     <img :src="song.data.album.images[0].url" class="w-100" alt="album cover" @load="loaded = true" />
                     <div class="card-body py-2 px-4 flex flex-row pt-6">
                         <div class="flex flex-col w-full">
                             <div class="flex flex-row items-center">
-                                <p v-if="song.index != undefined" class="text-3xl opacity-50">#{{ song.index + 1 }}</p>
-                                <div v-if="song.index != undefined" class="text-right flex flex-col items-end">
+                                <p v-if="index != undefined" class="text-3xl opacity-70">#{{ index + 1 }}</p>
+                                <div v-if="index != undefined" class="text-right flex flex-col items-end">
                                     <h2 :key="expanded" class="card-title ml-4 text-sm overflow-hidden">
                                         {{ shortText(song.data.name, 23) }}
                                     </h2>
@@ -34,12 +29,14 @@
                                 <div>
                                     Album: <span class="opacity-50">{{ shortText(song.data.album.name, 23) }}</span>
                                 </div>
-                                <div>
-                                    Top song since: <span class="opacity-50">{{ dateString(song.dateAdded) }}</span>
-                                </div>
-                                <div v-if="unique" class="text-error font-bold">Unpopular song!</div>
-                                <div v-if="!newSong() && oldest" class="text-secondary font-bold">Long time favuorite!</div>
                             </div>
+                            <div v-if="dateAdded != null">
+                                Top song since: <span class="opacity-50">{{ dateString(song.dateAdded) }}</span>
+                            </div>
+                            <div v-if="unique" class="text-error font-bold">Unpopular song!</div>
+                            <div v-if="newSong()" class="text-info font-bold">This is a new top song!</div>
+                            <div v-if="oldest == song.uuid" class="text-secondary font-bold">Long time favuorite!</div>
+
                             <button class="btn btn-sm btn-success w-44 mt-2" @click="openInSpotify">Open in spotify</button>
                         </div>
                     </div>
@@ -56,24 +53,24 @@ export default {
             type: Object,
             required: false,
         },
-        unique: {
-            type: Boolean,
-            required: false,
+        index: {
+            type: Number,
+            required: true,
         },
         oldest: {
-            type: Boolean,
+            type: String,
             required: false,
         },
     },
     data() {
         return {
             loaded: false,
-            // heights: ['90px', '215px'],
-            // height: '90px',
             initialHeight: 90,
             bigHeight: 215,
             expanded: false,
             height: 90,
+            previousIndex: null,
+            dateAdded: null,
         }
     },
     computed: {
@@ -83,15 +80,27 @@ export default {
                 width: '100%',
             }
         },
+        unique() {
+            return this.song.data.popularity <= 20
+        },
     },
-    mounted() {},
-    sockets: {},
+    watch: {
+        oldest() {},
+    },
+    mounted() {
+        this.$socket.client.emit('getStoredSongInformation', { uuid: this.song.uuid })
+    },
+    sockets: {
+        getStoredSongInformation({ uuid, item }) {
+            if (uuid === this.song.uuid) {
+                this.dateAdded = item.dateAdded
+                this.previousIndex = item.previousIndex[item.previousIndex.length - 1]
+            }
+        },
+    },
     methods: {
         openInSpotify() {
             window.open(this.song.data.external_urls.spotify, '_blank')
-        },
-        newSong() {
-            return new Date(this.song.dateAdded) > new Date() - 8.64e7 || this.song.previousIndex == null
         },
         shortText(text, l) {
             return text.length > l ? `${text.substr(0, l)}...` : text
@@ -109,7 +118,11 @@ export default {
                     newHeight += 22.5
                 }
 
-                if (this.song.previousIndex != null && this.oldest) {
+                if (this.newSong()) {
+                    newHeight += 22.5
+                }
+
+                if (this.oldest == this.song.uuid) {
                     newHeight += 22.5
                 }
 
@@ -117,6 +130,9 @@ export default {
             } else {
                 this.height = this.initialHeight
             }
+        },
+        newSong() {
+            return new Date() - new Date(this.dateAdded) < 30000000
         },
     },
 }
@@ -169,12 +185,13 @@ img {
 
 .fade-enter-active,
 .fade-leave-active {
-    transition: opacity 1s;
+    transition: all 1s;
 }
 
 .fade-enter,
 .fade-leave-to
 /* .fade-leave-active in <2.1.8 */ {
     opacity: 0;
+    transform: translateY(20px);
 }
 </style>
